8b1dd94016e94b02c2f9c29d4fe1c226c9f12e80
Austin Varnon <austinvarnon@gmail.com>
2020-09-30 11:13:59 -0500

    part 1 loop basics for loop refactors

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,14 @@
 public class ControlFlowExercises {
     public static void main (String[] args) {
-        int i = 2;
-        do {
+//        int i = 2;
+//        do {
+//            System.out.println(i);
+//            i += i;
+//        } while (i < 1000000);
+
+        for (int i = 5; i <= 15; i++){
             System.out.println(i);
-            i += i;
-        } while (i < 1000000);
+        }
 
 
 
