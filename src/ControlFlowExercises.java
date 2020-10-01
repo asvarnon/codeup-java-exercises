02b965178c4a925e6b85bed22b236e47435cdb05
Austin Varnon <austinvarnon@gmail.com>
2020-10-01 16:47:42 -0500

    refactored some ways to do this

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//        int i = 2;
+//        long i = 2;
 //        do {
 //            System.out.println(i);
 //            i += i;
-//        } while (i < 1000000);
+//        } while (i < 1000000L);
 
 //        for (int i = 2; i < 1000000; i += i){
 //            System.out.println(i);
