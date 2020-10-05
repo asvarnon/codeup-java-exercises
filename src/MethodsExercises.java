17731876c6e5810da1ee771e30fe3dcffd7be669
Austin Varnon <austinvarnon@gmail.com>
2020-10-05 10:15:56 -0500

    factorials complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,15 +3,25 @@ import java.util.Scanner;
 public class MethodsExercises {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
 //        System.out.println(Addition(20, 10));
 //        System.out.println(Subtraction(20, 10));
 //        System.out.println(Multiplication(4, 5));
 //        System.out.println(Division(20, 5));
 //        System.out.println(Remainder(10, 3));
 
-        System.out.println("Enter a number between 1 and 10.");
-        int userInput = getInteger(1, 10);
+//        int userInput = getInteger(1, 10);
+
+        while(true){
+            System.out.println("factorial() = " + factorial());
+            System.out.println("do you want to continue? y/n");
+            String answer = sc.nextLine();
+            if(answer.equalsIgnoreCase("n")){
+                System.out.println("bye bye");
+                break;
+            }
+        }
+
 
     }
 //    Scanner sc = new Scanner(System.in);
@@ -38,10 +48,13 @@ public class MethodsExercises {
 //    }
 
 
+
+
     public static int getInteger(int min, int max){
         Scanner sc = new Scanner(System.in);
         int userInput = sc.nextInt();
-        if (userInput > max || userInput < min){
+        System.out.println("Enter a number between " + min + " & " + max);
+        if (userInput >= max || userInput <= min){
             System.out.println("Invalid number, try again.");
             return getInteger(min, max);
         }
@@ -49,13 +62,16 @@ public class MethodsExercises {
         return userInput;
     }
 
-    public static long factorial(int number){
-        if (number <= 2){
-            System.out.println(number);
-            return number;
+    public static long factorial(){
+//        System.out.println("Enter number between 1 & 10");
+        int userInput = getInteger(1, 10);
+        long output = 1;
+        for (int counter = 1; counter <= userInput; counter++){
+            output *= counter;
         }
-        System.out.println("past if");
-        return number * factorial(number - 1);
+        return output;
     }
+
+
 
 }
