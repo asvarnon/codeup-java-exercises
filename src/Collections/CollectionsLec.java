728099d5523010936cb509bbf145c41b314b8a29
Austin Varnon <austinvarnon@gmail.com>
2020-10-13 10:04:39 -0500

    collections lec finished ArrayLists

--- src/Collections/CollectionsLec.java
+++ src/Collections/CollectionsLec.java
@@ -1,17 +1,36 @@
 package Collections;
 
+import oopLec.Post;
+
 import java.util.ArrayList;
 
 public class CollectionsLec {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>();
-        System.out.println(names.size());
-        names.add("Austin");
-        names.add("Joe");
-        names.add("Bob");
-        System.out.println(names.size());
+        ArrayList<Post> posts = new ArrayList<>();
+
+//        System.out.println(names.size());
+//        names.add("Austin");
+//        names.add("Joe");
+//        names.add("Bob");
+//        System.out.println(names.size());
+
+        posts.add(new Post("Austin's Weekend", "Austin"));
+        posts.add(new Post("Austin's Best Weekend", "Joe"));
+        posts.add(new Post("Austin's Latest Weekend", "Bob"));
 
 //        System.out.println(names.get(0));
-        System.out.println("names.get(names.indexOf(\"Bob\")).toUpperCase() = " + names.get(names.indexOf("Bob")).toUpperCase());
+//        System.out.println("names.get(names.indexOf(\"Bob\")).toUpperCase() = " + names.get(names.indexOf("Bob")).toUpperCase());
+
+        Post deletedPost = posts.remove(1);
+        deletedPost.printTitleAndAuthor();
+        System.out.println("posts.lastIndexOf(deletedPost) = " + posts.lastIndexOf(deletedPost));
+
+        if(!posts.isEmpty()){
+            for (Post post : posts){
+                post.printTitleAndAuthor();
+            }
+        }
+
     }
 }
