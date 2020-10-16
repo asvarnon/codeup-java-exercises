2bfce66a0ccc4ad5d6c1477fa32b9bda7ac122f3
Austin Varnon <austinvarnon@gmail.com>
2020-10-16 12:16:34 -0500

    finished files lec, still one tad problem

---
+++ src/files/FileIO.java
@@ -0,0 +1,63 @@
+package files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileIO {
+    public static Path createDirectoryAndFile(String directoryName, String fileName) throws IOException{
+        Path directoryPath = Paths.get(directoryName);
+        Path dataFilePath = Paths.get(directoryName, fileName);
+
+        //createFile makes an exception, added "throws" in Main. OR could wrap in try/catch.
+        //have to create a directory first before we create a file.
+
+        if (Files.notExists(directoryPath)) {
+            Files.createDirectories(directoryPath);
+        }
+        //OR
+        if (!Files.exists(dataFilePath)) {
+            Files.createFile(dataFilePath);
+        }
+
+        return dataFilePath;
+    }
+    public static void printFileContents(Path filePath) throws IOException{
+        List<String> fileContents = Files.readAllLines(filePath);
+        for (int i = 0; i < fileContents.size(); i++) {
+            System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
+        }
+    }
+
+    public static void updateLine(Path filePath, String oldValue, String newValue) throws IOException{
+        //Replace a line on the file.
+        List<String> fileContents.Files.readAllLines(filePath);
+        fileContents = Files.readAllLines(filePath);
+        List<String> modifiedList = new ArrayList<>();
+        for(String item : fileContents){
+            if (item.equalsIgnoreCase(oldValue)) {
+                // Add my modified item.
+                modifiedList.add(newValue);
+            }else {
+                // Add the existing item because it is not what we want to replace.
+                modifiedList.add(item);
+            }
+        }
+        Files.write(filePath, modifiedList);
+    }
+
+    public static void deleteLine(Path filePath, String lineToRemove) throws IOException{
+        List<String> fileContents = Files.readAllLines(filePath);
+        List<String> modifiedList = new ArrayList<>();
+        for(String item : fileContents){
+            //I want to remove bread
+            if(!item.equalsIgnoreCase(lineToRemove)){
+                modifiedList.add(item);
+            }
+        }
+        Files.write(filePath, modifiedList);
+    }
+}
--- src/files/FilesLec.java
+++ src/files/FilesLec.java
@@ -1,5 +1,6 @@
 package files;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,75 +15,36 @@ public class FilesLec {
         String directoryName = "data";
         String fileName = "grocery_list.txt";
 
-        Path directoryPath = Paths.get(directoryName);
-        Path dataFilePath = Paths.get(directoryName, fileName);
 
         //createFile makes an exception, added "throws" in Main. OR could wrap in try/catch.
         //have to create a directory first before we create a file.
 
         try {
-            if (Files.notExists(directoryPath)) {
-                Files.createDirectories(directoryPath);
-            }
-            //OR
-            if (!Files.exists(dataFilePath)) {
-                Files.createFile(dataFilePath);
-            }
+            Path.dataFilePath = FileIO.createDirectoryAndFile(directoryName, fileName);
 
             //lets write grocery list to the file.
             List<String> groceryList = Arrays.asList("Milk","Eggs","Bacon");
             Files.write(dataFilePath, groceryList);
 
-            List<String> fileContents = Files.readAllLines(dataFilePath);
-            for (int i = 0; i < fileContents.size(); i++) {
-                System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
-            }
+            FileIO.printFileContents(dataFilePath);
 
             //Append to the file.
             Files.write(dataFilePath, Arrays.asList("cereal", "bread"), StandardOpenOption.APPEND);
-            fileContents = Files.readAllLines(dataFilePath);
-            System.out.println();
-            for (int i = 0; i < fileContents.size(); i++) {
-                System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
-            }
+            FileIO.printFileContents(dataFilePath);
 
-            //Replace a line on the file.
-            fileContents = Files.readAllLines(dataFilePath);
-            List<String> modifiedList = new ArrayList<>();
-            for(String item : fileContents){
-                if (item.equalsIgnoreCase("milk")) {
-                    // Add my modified item.
-                    modifiedList.add("Whole Milk");
-                }else {
-                    // Add the existing item because it is not what we want to replace.
-                    modifiedList.add(item);
-                }
-            }
-            Files.write(dataFilePath, modifiedList);
-
-            fileContents = Files.readAllLines(dataFilePath);
-            System.out.println();
-            for (int i = 0; i < fileContents.size(); i++) {
-                System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
-            }
+            //replace a line on file
+            FileIO.updateLine(dataFilePath, "milk", "Whole Milk");
+            FileIO.printFileContents(dataFilePath);
 
             //remove a line from the file
-            fileContents = Files.readAllLines(dataFilePath);
-            System.out.println();
+            FileIO.deleteLine(dataFilePath, "bread");
+            FileIO.printFileContents(dataFilePath);
 
-            modifiedList = new ArrayList<>();
-            for(String item : fileContents){
-                //I want to remove bread
-                if(!item.equalsIgnoreCase("bread")){
-                    modifiedList.add(item);
-                }
-            }
-            Files.write(dataFilePath, modifiedList);
-            fileContents = Files.readAllLines(dataFilePath);
-            System.out.println();
-            for (int i = 0; i < fileContents.size(); i++) {
-                System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
-            }
+            //empty the list
+//            Files.write(dataFilePath, new ArrayList<>());
+//            System.out.println("after empty");
+//            FileIO.printFileContents(dataFilePath);
+
 
         } catch (IOException ex){
             System.out.println("Cannot Create the file");
